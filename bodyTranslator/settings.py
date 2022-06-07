70c47dd313a004b157f9cb8d32017811cbe64935
Kenneth Yashim <kzamani44@gmail.com>
2022-06-07 06:45:58 +0100

    import django_heroku

--- bodyTranslator/settings.py
+++ bodyTranslator/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-import django
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,7 +133,7 @@ USE_TZ = True
 
 #STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
 
